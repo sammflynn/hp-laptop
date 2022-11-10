70be26a5076935e89023abb9a68240100043368c
sammflynn <86454805+sammflynn@users.noreply.github.com>
2022-11-10 19:28:39 +0600

    Update sync_server.sh

--- sync_server.sh
+++ sync_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 GIT=`which git`
 ${GIT} submodule foreach ${GIT} pull origin main
-${GIT} pull --recurse-submodules https://github.com/sammflynn/hp-laptop
+${GIT} pull https://github.com/sammflynn/hp-laptop
 echo $(date +"%Y-%m-%d-%T") - simulation server environment variables and log file updated
