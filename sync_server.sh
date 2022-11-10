3329d668f6999b6bd9e3813efca77b5711accc24
sammflynn <sammflynn999@gmail.com>
2022-11-10 19:17:01 +0600

    sync

--- sync_server.sh
+++ sync_server.sh
@@ -2,3 +2,4 @@
 GIT=`which git`
 ${GIT} submodule update --remote --merge
 ${GIT} pull --recurse-submodules https://github.com/sammflynn/hp-laptop
+echo $(date +"%Y-%m-%d-%T") - simulation server environment variables and log file updated
